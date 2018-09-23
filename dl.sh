b5c66dee96bffe7da7c436a8c44f6b79ef288591
sisisin <azsisisin@gmail.com>
2018-09-23 18:13:34 +0900

    feat: dl from glitch server

---
+++ dl.sh
@@ -0,0 +1,23 @@
+readonly working_dir=$(cd "$(dirname "$0")" && pwd)
+
+curl "https://api.glitch.com/project/download/?authorization=$GLITCH_API_TOKEN&projectId=$project_id" | tar zx -C .
+
+glitch_ignore=(
+    .git
+    .data
+    .env
+)
+
+grep_arg=
+for pattern in "${glitch_ignore[@]}"; do
+    grep_arg+=" -e $pattern"
+done
+
+find "app" -type f | while read f; do
+    if ! echo $f | grep -sq $grep_arg; then
+        dist=$(echo $f | sed -E "s/app\///")
+        cp $f $dist
+    fi
+done
+
+rm -rf ./app
